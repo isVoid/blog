c6f1f60876c39ffcca4b69af562faeeafde33c6e
isVoid <michaelwang0905@icloud.com>
2019-10-20 11:18:47 -0400

    Updated gallery and generate picture thumbnail script

---
+++ script/genPicThumbnail.py
@@ -0,0 +1,36 @@
+from PIL import Image
+import os
+import glob
+
+height = 225.0
+width = -1 # Auto
+
+parent = './photos/'
+subcategories = ['cityscape', 'myself', 'naturalhistory']
+
+for cat in subcategories:
+# cat = 'naturalhistory'
+    imagedir = os.path.join(parent, cat)
+    thumbdir = os.path.join(imagedir, 'thumb')
+
+    imglist = glob.glob(os.path.join(imagedir, '*.jpg'))
+    thumblist = glob.glob(os.path.join(thumbdir, '*.jpg'))
+
+    imglist = [im.split('/')[-1] for im in imglist]
+    thumblist = [th.split('/')[-1] for th in thumblist]
+
+    existed_thumbs = [thn[6:] for thn in thumblist]
+
+    diff_list = set(imglist).difference(set(existed_thumbs))
+
+    log = cat + " new images: " + str(diff_list)
+    print(log)
+
+    for img in diff_list:
+        img_p = os.path.join(imagedir, img)
+        log = "     Generating: " + img_p
+        print(log)
+        im = Image.open(img_p)
+        ratio = height / im.height
+        im.thumbnail((im.width * ratio, height))
+        im.save(os.path.join(thumbdir, 'thumb_'+img))
